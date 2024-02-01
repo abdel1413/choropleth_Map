055df34b721220551a61a186c09155440b2b6a79
Your Name <you@example.com>
2024-02-01 05:43:08 -0500

    get the map and its attributes working

--- script.js
+++ script.js
@@ -4,20 +4,37 @@ const educationDataUrl =
 const countyDataUrl =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 
-console.log("d3", d3);
-console.log("topo", topojson);
 let educationData, countyData;
+
+const drawMap = () => {
+  const canvas = d3.select("#canvas");
+
+  canvas
+    .selectAll("path")
+    .data(countyData)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("class", "county")
+    .attr("fill", (d) => {
+      const id = d.id;
+      const percentage = educationData.find((d) => {
+        console.log("ee", d);
+      });
+    });
+
+  console.log("canvas", canvas);
+};
 
 d3.json(countyDataUrl).then((data) => {
   //   countyData = data;
-  console.log("d", data);
+
   countyData = topojson.feature(data, data.objects.counties).features;
-  console.log("ctd", countyData);
+  console.log("countd", countyData);
 
   d3.json(educationDataUrl).then((data) => {
-    // convert to geoJson()
-
     educationData = data;
-    console.log("eddata", data);
+    console.log("edc", educationData);
+    drawMap();
   });
 });
